738a7055f7dfea55ac45789af4ea0aa2a70f3951
Hazelette <62589987+Hazelette@users.noreply.github.com.>
2025-08-16 12:47:41 -0700

    Wrote logic to get the computer's choice in the rock-paper-scissors game.
    Using array and random to generate a random choice for the computer.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Test"); // ensures script is loaded
+// console.log("Test"); // ensures script is loaded
+
+// function to get computer's choice
+function getComputerChoice(){
+    // Array to store string of choices
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length); // generate random index between 0 and 2
+    return choices[randomIndex]; // return random choice from array
+    // using array is cleaner than using if-else statements
+}
+
+const computerChoice = getComputerChoice(); // call the function and store the result
+console.log(computerChoice); // test the function to ensure all 3 string are randomly selected
